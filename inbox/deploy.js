26a71fa27a5ad80e897c9f024d03820023d87782
mihailo_radojevic <radojevic.mihailo.00@gmail.com>
2022-06-03 10:06:19 +0200

    Codes I wrote. Without node_modules and packages.

---
+++ inbox/deploy.js
@@ -0,0 +1,23 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'cream order online cheap sorry shine seed more minute strong naive pig',
+  'https://rinkeby.infura.io/v3/c28fe31ecf5d48b4905d45673a0d1d83'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+     .deploy({data: '0x' + bytecode, arguments: ['Hi there!']})
+     .send({from: accounts[0]});
+
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
